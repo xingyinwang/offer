da51fad9a68cf08cc2735c3ac31c56a02ffff99a
xingyinwang@163.com <yin..0813>
2018-07-19 08:57:48 +0800

    add a new branch

---
+++ src/com/wxy/branchTest/BranchTestByKode.java
@@ -0,0 +1,10 @@
+package com.wxy.branchTest;
+
+/**
+ * Created by Kode on 2018/7/19.
+ */
+public class BranchTestByKode {
+    public static void main(String[] args){
+        System.out.println("这是分支：2018年7月19日08:56:04");
+    }
+}
